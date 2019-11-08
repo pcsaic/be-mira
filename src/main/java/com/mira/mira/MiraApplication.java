65cc88126dba9ee2ba8d57d2903f82be44944846
Alex <pcsaic@yandex.ru>
2019-11-09 00:16:25 +0300

    -first commit

---
+++ src/main/java/com/mira/mira/MiraApplication.java
@@ -0,0 +1,13 @@
+package com.mira.mira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiraApplication.class, args);
+	}
+
+}
